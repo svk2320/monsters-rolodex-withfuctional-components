0875ce8ec85bc1c4d89b438c8c1ca19f8fb6af17
username <vasanthkumars2025@gmail.com>
2023-03-05 16:34:52 +0530

    modified app.js

--- src/App.js
+++ src/App.js
@@ -7,23 +7,25 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Nithish'
+      monsters:[
+        {
+          name: 'm01'
+        },
+        {
+          name: 'm02'
+        },
+      ]
     }
   };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          {/* shallow merge -> completly changes the state values */}
-          <button onClick={() => {this.setState((state, props) => {
-            return {
-              name: 'Nithi'
-            }
-          }, () => {console.log(this.state)})}}>Change Name</button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>
+          })
+        }
       </div>
     );
   }
