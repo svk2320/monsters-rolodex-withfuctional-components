3c1854b657f980534ab61653ec65d251ca42dbff
username <vasanthkumars2025@gmail.com>
2023-03-06 08:42:32 +0530

    add css style to cards

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,24 @@
 import { Component } from "react";
+import './card-list.component.css';
 
 class CardList extends Component {
 
     render(){
         let { monsters } = this.props;
         return(
-            <div>
+            <div className="card-list">
                 {monsters.map((monster) => {
-                    return <h1 key={monster.id}>{monster.name}</h1>
+                    const { name, email, id} = monster;
+                    return(
+                        <div className="card-container" key={id}>
+                            <img 
+                                alt={`monster name: ${name}`} 
+                                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                            />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div> 
+                    )
                 })}
             </div>
         )
