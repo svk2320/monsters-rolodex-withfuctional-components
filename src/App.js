36fe5f95499981165e4b651b41c62c05a236ebe6
username <vasanthkumars2025@gmail.com>
2023-03-06 17:54:56 +0530

    modified files

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ class App extends Component {
     
     return (
       <div className="App">
+        <h1 className='app-title'>Wanted Monsters</h1>
         <SearchBox 
             className='monsters-search-box'
             onChangeHandler={ onSearchChange }
